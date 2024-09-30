be7b2d666d9016afb2357ca21046f6383673ad5a
Carlos Henrique klz <97392902+kralluz@users.noreply.github.com>
2024-09-30 15:52:04 -0300

    Resolve conflitos de merge

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
 
-export default app
+export default app;
