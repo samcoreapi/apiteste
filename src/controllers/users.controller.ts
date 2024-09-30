7f8ebf523f538976fa51e9be34db2074ded8a69b
Carlos Henrique klz <97392902+kralluz@users.noreply.github.com>
2024-09-30 17:04:43 -0300

    as

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -9,9 +9,9 @@ export class UsersController {
   }
 
   async createUser(req: Request, res: Response) {
-    const { name } = req.body;
+    const { name, password } = req.body;
     try {
-      const novoUsuario = await this.usersService.createUser(name);
+      const novoUsuario = await this.usersService.createUser(name, password);
       res.status(201).json(novoUsuario);
     } catch (error) {
       res.status(500).json({ message: "Erro ao criar usuário." });
@@ -19,9 +19,9 @@ export class UsersController {
   }
 
   async login(req: Request, res: Response) {
-    const { name } = req.body;
+    const { name, password } = req.body;
     try {
-      const token = await this.usersService.login(name);
+      const token = await this.usersService.login(name, password);
       res.json({ token });
     } catch (error) {
       res.status(401).json({ message: "Login falhou." });
