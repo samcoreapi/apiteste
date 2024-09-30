84ab313126de7dfcc6ed37340d3e14c7ef17ca8b
kralluz <chenrique201520@gmail.com>
2024-09-30 18:31:31 -0300

    a

--- src/services/session.service.ts
+++
@@ -1,7 +0,0 @@
-import { v4 as uuidv4 } from 'uuid'; // Para gerar um token simples
-
-export function generateToken(email: string): string {
-    // Neste exemplo, esta  mos usando um UUID como token
-    // Em uma aplicação real, você deve usar JWT ou outra forma de token seguro
-    return uuidv4();
-}
